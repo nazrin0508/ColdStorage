7ef91d0855e357b45bcb8f45851cc38dcf99f2e8
Lavanya <53055984+Lavi789@users.noreply.github.com>
2023-11-14 13:06:14 +0530

    added

--- coldstorage/hirer.php
+++ coldstorage/hirer.php
@@ -93,11 +93,11 @@ License: You must have a valid license purchased only from themeforest(the above
                       
             <!-- END: Content -->
         <!-- BEGIN: Dark Mode Switcher-->
-        <div data-url="side-menu-dark-crud-form.html" class="dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-0 box border rounded-full w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10">
+        <!-- <div data-url="side-menu-dark-crud-form.html" class="dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-0 box border rounded-full w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10">
             <div class="mr-4 text-slate-600 dark:text-slate-200">Dark Mode</div>
             <div class="dark-mode-switcher__toggle border"></div>
         </div>
-		</div>
+		</div> -->
         <!-- END: Dark Mode Switcher-->
         
         <!-- BEGIN: JS Assets-->
