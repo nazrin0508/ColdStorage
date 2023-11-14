33b0735186b881febcade85b15adc3cc712adf66
Deepa Coder <deepabharti9430@gmail.com>
2023-11-14 12:19:58 +0530

    add

---
+++ coldstorage/hirer.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<!--
+Template Name: Midone - HTML Admin Dashboard Template
+Author: Left4code
+Website: http://www.left4code.com/
+Contact: muhammadrizki@left4code.com
+Purchase: https://themeforest.net/user/left4code/portfolio
+Renew Support: https://themeforest.net/user/left4code/portfolio
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en" class="light">
+    <!-- BEGIN: Head -->
+    <head>
+        <meta charset="utf-8">
+        <link href="dist/images/logo.svg" rel="shortcut icon">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Midone admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
+        <meta name="keywords" content="admin template, Midone Admin Template, dashboard template, flat admin template, responsive admin template, web app">
+        <meta name="author" content="LEFT4CODE">
+        <title>CRUD Form - Midone - Tailwind HTML Admin Template</title>
+        <!-- BEGIN: CSS Assets-->
+        <link rel="stylesheet" href="dist/css/app.css" />
+        <!-- END: CSS Assets this is written by sneha -->
+    </head>
+    <!-- END: Head -->
+    <body class="py-5">
+		<div class="flex mt-[4.7rem] md:mt-0">
+		<?php include 'sidemenu.php' ?>
+            <!-- BEGIN: Content -->
+            <div class="content">
+			<?php include 'topbar.php' ?>
+                <div class="intro-y flex items-center mt-8">
+                    <h2 class="text-lg font-medium mr-auto">
+                    Form Layout
+                    </h2>
+                </div>
+                <div class="grid grid-cols-12 gap-6 mt-5">
+                    <div class="intro-y col-span-12 lg:col-span-12">
+                        <!-- BEGIN: Form Layout -->
+                        <div class="intro-y box p-5">
+                            <div>
+                                <label for="crud-form-1" class="form-label">Hirer Name</label>
+                                <input id="crud-form-1" type="text" class="form-control w-full" placeholder="Input text">
+                                
+                            </div>
+                            <div class="mt-3">
+                                <label for="crud-form-2" class="form-label">Father's Name</label>
+                                <input id="crud-form-1" type="text" class="form-control w-full" placeholder="Input text">
+                                
+                            </div>
+                            <div class="mt-3">
+                                <label for="crud-form-3" class="form-label">A/C off</label>
+                                <div class="input-group">
+                                    <input id="crud-form-3" type="text" class="form-control" placeholder="" aria-describedby="input-group-1">
+                                    
+                            </div>
+                            <div class="mt-3">
+                                <label for="crud-form-2" class="form-label">Phone No</label>
+                                <input id="crud-form-1" type="number" class="form-control w-full" placeholder="Input text">
+                                
+                            </div>
+                            
+
+                            
+                            <div class="mt-3">
+                            <label for="crud-form-4" class="form-label">Marketing</label>
+                                <select id="group-hirer" name="group_hirer" class="form-select w-full">
+                                    <option value="">Select an option</option>
+                                    <option value="1">Y</option>
+                                    <option value="2">N</option>
+                                    
+                                </select>
+                                </div>
+                                <div class="mt-3">
+                                <label for="crud-form-2" class="form-label">VoterId/PanNo</label>
+                                <input id="crud-form-1" type="text" class="form-control w-full" placeholder="Input text">
+                                
+                            </div>
+                            
+                            
+                            <div class="text-right mt-5">
+                                <button type="button" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
+                                <button type="button" class="btn btn-primary w-24">Save</button>
+                                <button type="button" class="btn btn-primary w-24">Update</button>
+                                <button type="button" class="btn btn-primary w-24">delete</button>
+                            </div>
+                        </div>
+                        <!-- END: Form Layout -->
+                    </div>
+                </div>
+            </div>
