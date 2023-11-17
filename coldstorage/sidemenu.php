d6e90e2012ce1a4b18a551a32c07fee5b384d50a
Lavanya <53055984+Lavi789@users.noreply.github.com>
2023-11-17 13:36:50 +0530

    added

--- coldstorage/sidemenu.php
+++ coldstorage/sidemenu.php
@@ -793,7 +793,7 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="deliveryslip.php.php" class="side-menu">
+                                <a href="deliveryslip.php." class="side-menu">
                                     <div class="side-menu__icon"> <i data-lucide="activity"></i> </div>
                                     <div class="side-menu__title"> Delivery Slip </div>
                                 </a>
